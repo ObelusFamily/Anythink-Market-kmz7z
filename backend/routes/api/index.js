0cbdc88e452124df778c7c9af230999912917bd4
Sairaj Yadav <67747523+yadavsairaj@users.noreply.github.com>
2022-08-24 15:56:48 +0530

    Update index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,7 @@ router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/items', require('./items'));
 router.use('/tags', require('./tags'));
-router.use('/tags', require('./products'));
+router.use('/products', require('./tags'));
 router.use('/ping', require('./ping'));
 
 router.use(function(err, req, res, next){
